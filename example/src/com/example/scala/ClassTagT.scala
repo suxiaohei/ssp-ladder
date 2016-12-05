615317e70aac9e1c2f388a31b0ec1d2674f4a71d
suxiaohei <shupaiqianbi@qq.com>
2016-12-05 14:58:21 +0800

    add  test

---
+++ example/src/com/example/scala/ClassTagT.scala
@@ -0,0 +1,15 @@
+package com.example.scala
+
+import scala.reflect.ClassTag
+import scala.concurrent.Future
+
+/**
+  * Created by suxin on 16-12-5.
+  */
+class ClassTagT[ACT: ClassTag] {
+
+  type Class_Type = ACT
+
+  //val
+}
+
